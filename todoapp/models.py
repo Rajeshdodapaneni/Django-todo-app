c2447313566ea2ef254dce465c1ee53c70cafbc7
Dodapaneni Rajesh <rajeshdodapaneni055@gmail.com>
2025-12-31 12:29:49 +0530

    file upload

--- todoapp/models.py
+++ todoapp/models.py
@@ -6,6 +6,7 @@ class todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     todo_name = models.CharField(max_length=1000)
     status = models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.todo_name
